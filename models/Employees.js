7f3a65f4529eb5d90dcf2fc63a597250101ce2a7
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-02-15 16:19:31 +0200

    made the salary and job title not required field

--- models/Employees.js
+++ models/Employees.js
@@ -1,18 +1,20 @@
-import mongoose from "mongoose"
-const { Schema, model} = mongoose
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
 
-const employeeSchema = new Schema({
+const employeeSchema = new Schema(
+  {
     fullname: { type: String, required: true },
-    email: { type: String,required: true, unique: true },
-    password: {type: String, required: true},
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     phone: { type: String, required: true },
-    address: { type: String,required: true},
-    jobTitle: {type: String, required: true},
-    role:{type: String, enum: ['admin', 'employee'], default: 'employee'}, //use this for role based access
-    salary: { type: Number, required: true},
-    leads:[{type: Schema.Types.ObjectId, ref: 'Lead'}], //reference to leads
-    clients:[{type: Schema.Types.ObjectId, ref: 'Client'}] //reference to clients
+    address: { type: String, required: true },
+    jobTitle: { type: String },
+    role: { type: String, enum: ["admin", "employee"], default: "employee" }, //use this for role based access
+    salary: { type: Number },
+    leads: [{ type: Schema.Types.ObjectId, ref: "Lead" }], //reference to leads
+    clients: [{ type: Schema.Types.ObjectId, ref: "Client" }], //reference to clients
+  },
+  { timestamps: true }
+);
 
-}, { timestamps: true })
-
-export const Employee = model('Employee', employeeSchema)
+export const Employee = model("Employee", employeeSchema);
