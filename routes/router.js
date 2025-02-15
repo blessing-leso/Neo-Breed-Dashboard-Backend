14e7b6f6ab71170b87469511e7240c831ac22b51
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-02-15 16:18:13 +0200

    added the router to update employee selected fields

--- routes/router.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express'
-const router = Router()
-import {registerEmployee} from '../controllers/controller.js'
-import {login} from '../controllers/authController.js'
-
-router.post('/register', registerEmployee)
-router.get('/auth/login', login)
-
-export default router
