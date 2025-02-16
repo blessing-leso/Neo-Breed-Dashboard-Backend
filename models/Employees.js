34071d7e6217fd8acedc82348f51fbe91f35db5f
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-02-16 17:01:53 +0200

    added the method for creating password reset token

--- models/Employees.js
+++ models/Employees.js
@@ -9,6 +9,8 @@ const employeeSchema = new Schema(
     phone: { type: String, required: true },
     address: { type: String, required: true },
     jobTitle: { type: String },
+    passwordResetToken: String,
+    passwordResetExpires: String,
     role: { type: String, enum: ["admin", "employee"], default: "employee" }, //use this for role based access
     salary: { type: Number },
     leads: [{ type: Schema.Types.ObjectId, ref: "Lead" }], //reference to leads
@@ -16,5 +18,18 @@ const employeeSchema = new Schema(
   },
   { timestamps: true }
 );
+
+employeeSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
 
 export const Employee = model("Employee", employeeSchema);
