b1ea6234fb9a9facd60f03859ef07505199fe45c
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-02-16 18:14:36 +0200

    changed naming from getEmployees to getAllEmployees

--- controllers/controller.js
+++ controllers/controller.js
@@ -63,7 +63,7 @@ export const registerClient = async(req,res) => {
     }
 }
 
-export const getEmployees = async(req,res) => {
+export const getAllEmployees = async(req,res) => {
     try {
         const employees = await Employee.find()
         res.status(200).json(employees)
--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,11 @@
 import {Router} from 'express'
 const router = Router()
-import {registerEmployee, registerLead, registerClient, getEmployees, getLeads, getClients} from '../controllers/controller.js'
+import {registerEmployee, registerLead, registerClient, getAllEmployees, getLeads, getClients} from '../controllers/controller.js'
 import {login, authenticateToken, authorizeRoles} from '../controllers/authController.js'
 
 // Get requests
 router.get('/auth/login', login)
-router.get('/auth/employees', authenticateToken, authorizeRoles('admin'), getEmployees)
+router.get('/auth/employees', authenticateToken, authorizeRoles('admin'), getAllEmployees)
 router.get('/auth/leads', authenticateToken, getLeads)
 router.get('/auth/clients', authenticateToken, getClients)
 
