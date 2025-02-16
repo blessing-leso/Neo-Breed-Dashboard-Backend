d4c50afbf29004c10314487e524fac6b1eb05bc3
FHATUWANI Mulaudzi <111020728+Mulaudzi-F@users.noreply.github.com>
2025-02-17 00:04:30 +0200

    Revert "added the API controller for updating the employee"

--- controllers/leadController.js
+++
@@ -1,4 +0,0 @@
-import Lead from "../models/leadModel.js";
-import { createOne } from "./handleFactory.js";
-
-export const createLead = createOne(Lead);
--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-import express, { json } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import "dotenv/config";
-import { connectDB } from "./config/db.js";
-import router from "./routes/EmployeeRouter.js";
+import express, {json} from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import 'dotenv/config'
+import {connectDB} from './config/db.js'
+import router from './routes/router.js'
 
-const server = express();
+
+const server = express()
 
 //middlewares
-server.use(cors());
-server.use(json());
-server.use(cookieParser());
-server.use("/api", router);
+server.use(cors())
+server.use(json())
+server.use(cookieParser())
+server.use('/api',router)
 
 //connect to database
-connectDB();
+connectDB()
 
 server.listen(process.env.PORT, () => {
-  console.log(
-    `Server is running on port http://localhost:${process.env.PORT || 3000}`
-  );
-});
+    console.log(`Server is running on port http://localhost:${process.env.PORT || 3000}`)
+})
