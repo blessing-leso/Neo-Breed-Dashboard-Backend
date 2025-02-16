a14d5bbca87500f5ad60fd64d5b23a189a5222c9
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-02-16 17:46:35 +0200

    changed to  the ES6 for imports

--- utils/email.js
+++ utils/email.js
@@ -1,4 +1,4 @@
-const nodemailer = require("nodemailer");
+import nodemailer from "nodemailer";
 
 const sendEmail = async (options) => {
   //(1) create Transporter
@@ -24,4 +24,4 @@ const sendEmail = async (options) => {
   await transporter.sendMail(mailOptions);
 };
 
-module.exports = sendEmail;
+export default sendEmail;
