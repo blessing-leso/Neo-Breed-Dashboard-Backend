c39e52a018baaf849b5ddd4511116589139b2d20
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-02-17 13:00:23 +0200

    added all request function

--- controllers/leadController.js
+++ controllers/leadController.js
@@ -1,1 +1,8 @@
+import { getOne } from "./handleFactory";
+import Lead from "../models/leadModel";
 
+const createLead = getOne(Lead);
+const getLead = getOne(Lead);
+const getAllLeads = getAll(Lead);
+const updateLead = updateOne(Lead);
+const deleteLead = deleteOne(Lead);
