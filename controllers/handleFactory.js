f067fec8c56d5a621f15e1f16ebc7b8f0bd96325
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-02-17 13:59:58 +0200

    added the handle factory function to delete

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -35,3 +35,16 @@ export const updateOne = (Model) => {
     res.status(200).json({ doc });
   });
 };
+
+export const deleteOne = (Model) => {
+  CatchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+    if (!doc) {
+      return next(new AppError("No document found with that ID", 404));
+    }
+
+    res.status(204).json({
+      data: null,
+    });
+  });
+};
