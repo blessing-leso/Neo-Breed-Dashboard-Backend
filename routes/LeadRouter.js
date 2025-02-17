f7f712ff0ecf6bde5e1c909467c274761aed7f95
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-02-17 14:11:19 +0200

    added the routes to make the requests to all our lead api

--- routes/LeadRouter.js
+++ routes/LeadRouter.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 const router = Router();
-import {
-    authenticateToken,
-  } from "../controllers/authController.js";
+import { authenticateToken } from "../controllers/authController.js";
 
-import {registerLead, getLeads} from '../controllers/leadController.js'
-router.get('/auth/leads', authenticateToken, getLeads)
-router.post('/register/lead', authenticateToken,registerLead)
+import {
+  registerLead,
+  getLeads,
+  getAllLeads,
+  createLead,
+  updateLead,
+  deleteLead,
+} from "../controllers/leadController.js";
+router.get("/auth/leads", authenticateToken, getAllLeads);
+router.post("/register/lead", authenticateToken, createLead);
+router.patch("/update/lead", authenticateToken, updateLead);
+router.get("/getLead/:id", authenticateToken, getLeads);
+router.delete("/delete/lead", authenticateToken, deleteLead);
