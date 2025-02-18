355848154d7850a7e016b9e8b40a7bd90a40fa8b
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-02-18 13:30:55 +0200

    accepted both incoming and current data from the remote repo and local repo

--- routes/EmployeeRouter.js
+++ routes/EmployeeRouter.js
@@ -8,28 +8,30 @@ import {
   forgotPassword,
   getAllEmployees,
   resetPassword,
-  getEmployeeWithDetails
+  getEmployeeWithDetails,
 } from "../controllers/EmployeeController.js";
 import {
   authenticateToken,
   login,
   authorizeRoles,
-  logout
-  } from "../controllers/authController.js";
+  logout,
+} from "../controllers/authController.js";
 
 router.post("/register", registerEmployee);
 router.get("/auth/login", login);
 router.post("/forgotpassword", forgotPassword);
 
-router.get('/auth/employees', authenticateToken, getAllEmployees)
-router.get('/auth/employees-details/', authenticateToken, getEmployeeWithDetails)
-router.get('/auth/logout', logout)
+router.get("/auth/employees", authenticateToken, getAllEmployees);
+router.get(
+  "/auth/employees-details/",
+  authenticateToken,
+  getEmployeeWithDetails
+);
+router.get("/auth/logout", logout);
 
 router.patch("/updateEmployee/:id", authenticateToken, updateEmployee);
 router.patch("/resetPassword/:token", resetPassword);
 router.delete("/deleteEmployee/:id", authenticateToken, deleteEmployee);
-router.get("getMe", authenticateToken, getMe);
+router.get("/getMe", authenticateToken, getMe);
 
 export default router;
-
-
