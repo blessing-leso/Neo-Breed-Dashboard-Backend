c5cd0675620517a3d5e6d0d66f8a6be878187d6a
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-02-19 10:06:16 +0200

    Accepted incoming changes

--- routes/EmployeeRouter.js
+++ routes/EmployeeRouter.js
@@ -20,17 +20,6 @@ import {
   
 // GET requests 
 router.get("/auth/login", login);
-<<<<<<< HEAD
-router.post("/forgotpassword", forgotPassword);
-
-router.get("/auth/employees", authenticateToken, getAllEmployees);
-router.get(
-  "/auth/employees-details/",
-  authenticateToken,
-  getEmployeeWithDetails
-);
-router.get("/auth/logout", logout);
-=======
 router.get('/auth/employees', authenticateToken,authorizeRoles('Admin', 'HR', 'Manager'), getAllEmployees)
 router.get('/auth/employees-details/', authenticateToken, getEmployeeWithDetails)
 router.get('/auth/logout', logout)
@@ -39,7 +28,6 @@ router.get("getMe", authenticateToken, getMe);
 // POST requests  
 router.post("/register", registerEmployee);
 router.post("/forgotpassword", forgotPassword);
->>>>>>> main
 
 // PATCH requests
 router.patch("/updateEmployee/:id", authenticateToken, updateEmployee);
@@ -47,9 +35,5 @@ router.patch("/resetPassword/:token", resetPassword);
 
 // DELETE requests
 router.delete("/deleteEmployee/:id", authenticateToken, deleteEmployee);
-<<<<<<< HEAD
-router.get("/getMe", authenticateToken, getMe);
-=======
->>>>>>> main
 
 export default router;
