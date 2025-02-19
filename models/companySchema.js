f35b5600893a88503f1fc61f71549d17aec613dc
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-02-19 16:19:29 +0200

    Created the schema for company

---
+++ models/companySchema.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const { model, Schema } = mongoose;
+
+const CompanySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  employees: [],
+  client: [],
+  Lead: [],
+});
+
+export const Company = model(Company, "CompanySchema");
