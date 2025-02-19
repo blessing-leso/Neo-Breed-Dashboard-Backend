af13a0825bdcc4aa84fb418c1593d0b7adc3385c
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-02-19 21:24:45 +0200

    added the extentions of the files i imported form

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,11 +1,11 @@
-import { Company } from "../models/companySchema";
+import { Company } from "../models/companySchema.js";
 import {
   createOne,
   deleteOne,
   getAll,
   getOne,
   updateOne,
-} from "./handleFactory";
+} from "./handleFactory.js";
 
 export const createCompany = createOne(Company);
 export const getCompanies = getAll(Company);
