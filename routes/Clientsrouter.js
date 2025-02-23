f7a20b40c8e135cf4be50f4310ed503cf0340a0f
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-02-23 16:49:44 +0200

    changed the route for creating client

--- routes/Clientsrouter.js
+++ routes/Clientsrouter.js
@@ -1,20 +1,46 @@
 import { Router } from "express";
-const router = Router();
-import {authenticateToken, authorizeRoles} from "../controllers/authController.js";
-import {registerClient, getClients, assignClient, getClient, unassignClient, deleteClient} from '../controllers/clientController.js'
+const router = Router({ mergeParams: true });
+import {
+  authenticateToken,
+  authorizeRoles,
+} from "../controllers/authController.js";
+import {
+  registerClient,
+  getClients,
+  assignClient,
+  getClient,
+  unassignClient,
+  deleteClient,
+} from "../controllers/clientController.js";
+import { setCompanyId } from "../controllers/EmployeeController.js";
 
 // GET requests
-router.get('/client/get-clients', authenticateToken, getClients)
-router.get('/client/get-client', authenticateToken, getClient)
+router.get("/client/get-clients", authenticateToken, getClients);
+router.get("/client/get-client", authenticateToken, getClient);
 
 // POST requests
-router.post('/register/client', authenticateToken,registerClient)
-router.post('/client/assign-client', authenticateToken, authorizeRoles('Admin', 'HR', 'Manager') , assignClient)
+router.post("/", authenticateToken, setCompanyId, registerClient);
+router.post(
+  "/client/assign-client",
+  authenticateToken,
+  authorizeRoles("Admin", "HR", "Manager"),
+  assignClient
+);
 
 // DELETE requests
-router.delete('/client/delete-client', authenticateToken, authorizeRoles('Admin', 'HR', 'Manager'), deleteClient)
+router.delete(
+  "/client/delete-client",
+  authenticateToken,
+  authorizeRoles("Admin", "HR", "Manager"),
+  deleteClient
+);
 
 // PATCH requests
-router.patch('/client/unassign-client', authenticateToken, authorizeRoles('Admin', 'HR', 'Manager'), unassignClient)
+router.patch(
+  "/client/unassign-client",
+  authenticateToken,
+  authorizeRoles("Admin", "HR", "Manager"),
+  unassignClient
+);
 
-export default router
+export default router;
