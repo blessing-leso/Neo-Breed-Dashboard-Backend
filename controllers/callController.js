3e9ee94fb3ae496bb3aec5c244908ce12feed99a
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-02-25 04:00:29 +0200

    added a middleware function that gets the the client id from the params

--- controllers/callController.js
+++ controllers/callController.js
@@ -1,6 +1,12 @@
 import { Call } from "../models/CallSchema.js";
 import { createOne, deleteOne, getAll, getOne } from "./handleFactory.js";
 
+export const setClientandEmployeeId = (req, res, next) => {
+  if (!req.body.client) req.body.client = req.params.id;
+  //if (!req.body.employee) req.body.employee = req.employee.id;
+  next();
+};
+
 export const createCall = createOne(Call);
 export const getAllCalls = getAll(Call);
 export const deleteCall = deleteOne(Call);
